76f503b93d0849aa97c47ab7ad94c456eede7b5b
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-04-17 13:45:29 +0300

    Script to find explicitly installed packages

---
+++ find-explicitly-installed-packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for PACKAGE in $(paru -Q | awk '{print $1}'); do
+    if paru -Qi "${PACKAGE}" | grep -q "Optional For\s*:\s*None$" \
+    && paru -Qi "${PACKAGE}" | grep -q "Required By\s*:\s*None$"; then
+        echo "${PACKAGE}"
+    fi
+done
