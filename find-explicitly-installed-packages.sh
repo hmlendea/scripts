eb25a19bb6fd6728866955e4ff12eb021d9d35e0
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-05-27 14:24:34 +0300

    Improved script

--- find-explicitly-installed-packages.sh
+++ find-explicitly-installed-packages.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 for PACKAGE in $(paru -Q | awk '{print $1}'); do
-    if paru -Qi "${PACKAGE}" | grep -q "Optional For\s*:\s*None$" \
-    && paru -Qi "${PACKAGE}" | grep -q "Required By\s*:\s*None$"; then
+    #if paru -Qi "${PACKAGE}" | grep -q "Optional For\s*:\s*None$" \
+    #&& paru -Qi "${PACKAGE}" | grep -q "Required By\s*:\s*None$"; then
+    if paru -Qi "${PACKAGE}" | grep -q "Required By\s*:\s*None$"; then
         echo "${PACKAGE}"
     fi
 done
